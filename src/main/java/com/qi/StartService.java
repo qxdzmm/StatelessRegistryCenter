7d7ce4c25768b07a3a5544262837cda16768da76
qixiaodong <qixiaodong@PFL-0202000004.corp.ppdai.com>
2019-11-05 14:13:46 +0800

    项目结构搭建

---
+++ src/main/java/com/qi/StartService.java
@@ -0,0 +1,12 @@
+package com.qi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartService.class, args);
+	}
+}
---
+++ src/main/java/com/qi/core/TTT.java
@@ -0,0 +1,5 @@
+package com.qi.core;
+
+public class TTT {
+
+}
